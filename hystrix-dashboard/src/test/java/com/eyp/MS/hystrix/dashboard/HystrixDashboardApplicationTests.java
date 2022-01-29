fb76cfd2ff7fe7b09555031492ad1a2ff9ec1543
eyp <eypsay@yahoo.com>
2022-01-29 11:38:30 +0300

    Hystrix project stared

---
+++ hystrix-dashboard/src/test/java/com/eyp/MS/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyp.MS.hystrix.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
